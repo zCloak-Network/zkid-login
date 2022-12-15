26f532e0a373d2003b4af800dc52595b8e1dfdbc
Zhichao Zhang <zzcwoshizz@163.com>
2022-12-15 16:37:51 +0800

    fromProvider static method

--- packages/did/src/LoginDid.ts
+++ packages/did/src/LoginDid.ts
@@ -9,15 +9,42 @@ import type {
   SignedData
 } from '@zcloak/did/types';
 import type { DidUrl } from '@zcloak/did-resolver/types';
+import type { BaseProvider } from '@zcloak/login-providers/base/Provider';
 
 import { hexToU8a } from '@polkadot/util';
 
 import { Did } from '@zcloak/did';
-import { BaseProvider } from '@zcloak/login-providers/base/Provider';
+import { parseDidDocument } from '@zcloak/did/did/helpers';
 
 type HexString = `0x${string}`;
 
 export class LoginDid extends Did implements IDidKeyring {
+  /**
+   * @name fromProvider
+   * @description
+   * init did instance from `provider`
+   * @example
+   * ```typescript
+   * import { LoginDid } from '@zcloak/login-did'
+   *
+   * const did = await LoginDid.from(provider);
+   *
+   * await did.sign(...); // sign with did login
+   * await did.encrypt(...); // encrypt with did login
+   * ```
+   */
+  public static async fromProvider(provider: BaseProvider) {
+    if (!(await provider.isAuth())) {
+      await provider.requestAuth();
+    }
+
+    const { document } = await provider.getCurrentDid();
+
+    const didDetails = parseDidDocument(document);
+
+    return new LoginDid(didDetails, provider);
+  }
+
   public provider: BaseProvider;
 
   constructor(didDetails: IDidDetails, provider: BaseProvider) {
